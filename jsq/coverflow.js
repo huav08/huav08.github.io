d002c3cac6a961e394816eeb98cfbd1c060c2257
vhua06 <vhua06@gmail.com>
2026-01-15 14:27:00 +0800

    U 輪播圖

--- jsq/coverflow.js
+++ jsq/coverflow.js
@@ -3,8 +3,9 @@ var swiper = new Swiper(".mySwiper", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  initialSlide: 2, // 從第2個 slide 開始顯示
-  loop: true, // 讓輪播具備無限循環的功能
+  initialSlide: 2,
+  loop: true,
+  speed: 600,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -12,11 +13,19 @@ var swiper = new Swiper(".mySwiper", {
     modifier: 1,
     slideShadows: true,
   },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  navigation: { // 啟用左右切換按鈕
+  navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  keyboard: {
+    enabled: true,
   },
 });

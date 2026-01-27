d66b490194557b06a643728b032819cc6fc5e41c
vhua06 <vhua06@gmail.com>
2026-01-27 10:33:20 +0800

    u bg anim

---
+++ jsq/blobs-anim.js
@@ -0,0 +1,14 @@
+// Animation is now handled by CSS for better performance.
+// This file is kept empty to prevent JS errors if referenced, 
+// or you can remove the script tag from HTML.
+
+/*
+// Original JS logic was causing performance issues.
+class Blob {
+    constructor(el) {
+        // ...
+    }
+    // ...
+}
+initBlobs();
+*/
